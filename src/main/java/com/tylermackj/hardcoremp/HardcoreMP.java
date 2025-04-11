e9d1face479bfe73c1b1af378e8542cd1a04e511
TylerMackJ <tylermackj+github@proton.me>
2025-04-10 19:49:20 -0500

    new stuff

--- src/main/java/com/tylermackj/hardcoremp/HardcoreMP.java
+++ src/main/java/com/tylermackj/hardcoremp/HardcoreMP.java
@@ -4,10 +4,12 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.entity.event.v1.ServerLivingEntityEvents;
+import net.fabricmc.fabric.api.entity.event.v1.ServerPlayerEvents;
 import net.fabricmc.fabric.api.networking.v1.PlayerLookup;
 
 import org.slf4j.Logger;
@@ -20,6 +22,19 @@ public class HardcoreMP implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		ServerPlayerEvents.AFTER_RESPAWN.register((oldPlayer, newPlayer, alive) -> {
+			LOGGER.info("Player respawning " + newPlayer.getName() );
+
+			BlockPos spawnPoint = new BlockPos((int) oldPlayer.getPos().getX(), (int) oldPlayer.getPos().getY(), (int) oldPlayer.getPos().getZ());
+			spawnPoint = spawnPoint.east(1000);
+
+			while (!oldPlayer.getWorld().isSkyVisible(spawnPoint)){
+				spawnPoint = spawnPoint.up(1);
+			}
+
+			LOGGER.info("Teleport?: " + newPlayer.teleport(spawnPoint.getX(), spawnPoint.getY(), spawnPoint.getZ(), true));
+		});
+
 		ServerLivingEntityEvents.AFTER_DEATH.register((entity, cause) -> {
 			if (entity.isPlayer()) {
 				LOGGER.info("Player died " + entity.getName() );
@@ -29,8 +44,6 @@ public class HardcoreMP implements ModInitializer {
 						LOGGER.info("Killing " + player.getName());
 						player.damage((ServerWorld) player.getWorld(), player.getDamageSources().playerAttack((PlayerEntity) entity), Float.MAX_VALUE);
 					}
-
-					player.setSpawnPoint(player.Respawn, );
 				});
 			}
 		});
