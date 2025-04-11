31f6d9f202af7eeca38039739dd2dee9fde439b9
TylerMackJ <tylermackj+github@proton.me>
2025-04-10 21:34:08 -0500

    Respawn working

--- src/main/java/com/tylermackj/hardcoremp/HardcoreMP.java
+++ src/main/java/com/tylermackj/hardcoremp/HardcoreMP.java
@@ -1,16 +1,20 @@
 package com.tylermackj.hardcoremp;
 
-import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.tag.TagKey;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
-
+import net.minecraft.world.biome.Biome;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.entity.event.v1.ServerLivingEntityEvents;
 import net.fabricmc.fabric.api.entity.event.v1.ServerPlayerEvents;
 import net.fabricmc.fabric.api.networking.v1.PlayerLookup;
+import net.minecraft.util.Identifier;
+
+import java.util.Random;
+import java.util.function.Predicate;
+import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,19 +24,39 @@ public class HardcoreMP implements ModInitializer {
 
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+	public static final Random random = new Random();
+
+	public static BlockPos randomBlockPos() {
+		int randomSize = 1000000;
+		return new BlockPos(
+			random.nextInt(-randomSize, randomSize),
+			1024,	
+			random.nextInt(-randomSize, randomSize)
+		);
+	}
+
 	@Override
 	public void onInitialize() {
 		ServerPlayerEvents.AFTER_RESPAWN.register((oldPlayer, newPlayer, alive) -> {
 			LOGGER.info("Player respawning " + newPlayer.getName() );
+			Optional<BlockPos> finalSpawnPoint = Optional.ofNullable(null);
+			while (finalSpawnPoint.isEmpty()) {
+				LOGGER.info("Generating spawnpoint");
+				BlockPos spawnPoint = randomBlockPos();
 
-			BlockPos spawnPoint = new BlockPos((int) oldPlayer.getPos().getX(), (int) oldPlayer.getPos().getY(), (int) oldPlayer.getPos().getZ());
-			spawnPoint = spawnPoint.east(1000);
+				TagKey<Biome> oceanTag = TagKey.of(RegistryKeys.BIOME, Identifier.of("c:is_ocean"));
 
-			while (!oldPlayer.getWorld().isSkyVisible(spawnPoint)){
-				spawnPoint = spawnPoint.up(1);
+				while (newPlayer.getWorld().getBiome(spawnPoint).isIn(oceanTag)) {
+					LOGGER.info("Avoiding ocean");
+					spawnPoint = randomBlockPos();
+				}
+
+				finalSpawnPoint = BlockPos.findClosest(spawnPoint, 0, 1024, (blockPos) -> {
+					LOGGER.info("Finding ground");
+					return !newPlayer.getWorld().isAir(blockPos.down());
+				});
 			}
-
-			LOGGER.info("Teleport?: " + newPlayer.teleport(spawnPoint.getX(), spawnPoint.getY(), spawnPoint.getZ(), true));
+			newPlayer.requestTeleport(finalSpawnPoint.get().getX(), finalSpawnPoint.get().getY(), finalSpawnPoint.get().getZ());
 		});
 
 		ServerLivingEntityEvents.AFTER_DEATH.register((entity, cause) -> {
