47e33c38b3d916221d4b24a65f55a571b86a1f17
TylerMackJ <tylermackj+github@proton.me>
2025-04-17 18:59:27 -0500

    Updated join team display to be in alphabetical order

--- src/main/java/com/tylermackj/hardcoremp/handlers/JoinHandler.java
+++ src/main/java/com/tylermackj/hardcoremp/handlers/JoinHandler.java
@@ -1,7 +1,11 @@
 package com.tylermackj.hardcoremp.handlers;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+
 import com.tylermackj.hardcoremp.HardcoreMP;
 import com.tylermackj.hardcoremp.utils.Utils;
+import com.tylermackj.hardcoremp.utils.WordProvider.NamePair;
 
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerEntityEvents;
 import net.minecraft.scoreboard.Team;
@@ -13,26 +17,45 @@ import net.minecraft.text.Style;
 import net.minecraft.text.Text;
 
 public class JoinHandler {
+    private static class NamePair implements Comparable<NamePair> {
+        Text displayName;
+        String name;
+
+        public NamePair(Text displayName, String name) {
+            this.displayName = displayName;
+            this.name = name;
+        }
+
+        @Override
+        public int compareTo(NamePair o) {
+            return this.displayName.getLiteralString().toLowerCase().compareTo(o.displayName.getLiteralString().toLowerCase());
+        }
+    }
 
     public static void registerEvents() {
         ServerEntityEvents.ENTITY_LOAD.register((entity, world) -> {
             if (entity.isPlayer()) {
                 HardcoreMP.LOGGER.info("Player joined " + entity.getName());
-                MutableText teamOptionsText = Text.of("Join team:").copy();
+                ArrayList<NamePair> teamNames = new ArrayList<>();
                 for (Team team : world.getScoreboard().getTeams()) {
+                    teamNames.add(new NamePair(team.getDisplayName(), team.getName()));
+                }
+                teamNames.sort(null);
+                MutableText teamOptionsText = Text.of("Join team:").copy();
+                for (NamePair name : teamNames) {
                     teamOptionsText.append(
                         Text.of("\n    ").copy().append(
-                            team.getDisplayName().copy().setStyle(
-                                team.getDisplayName().getStyle().withClickEvent(
-                                    new ClickEvent.RunCommand("/hardcore-mp join " + team.getName())
+                            name.displayName.copy().setStyle(
+                                name.displayName.getStyle().withClickEvent(
+                                    new ClickEvent.RunCommand("/hardcore-mp join " + name.name)
                                 ).withHoverEvent(
-                                    new HoverEvent.ShowText(Text.of("Join ").copy().append(team.getDisplayName()))
+                                    new HoverEvent.ShowText(Text.of("Join ").copy().append(name.name))
                                 ).withUnderline(true)
                             )
                         )
                     );
                 }
-                MutableText teamCreateText = Text.of("Create new team").copy().append(
+                MutableText teamCreateText = Text.of("Create team:").copy().append(
                     Text.of("\n    ").copy().append(
                         Text.of("Manual").copy().setStyle(
                             Style.EMPTY.withClickEvent(new ClickEvent.SuggestCommand("/hardcore-mp create ")).withUnderline(true)
