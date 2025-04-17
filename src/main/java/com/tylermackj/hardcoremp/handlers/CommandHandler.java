04d581752fade9156b80c71ae7042bf167e7bf9e
TylerMackJ <tylermackj+github@proton.me>
2025-04-17 18:26:08 -0500

    Added more custom wrapper commands

--- src/main/java/com/tylermackj/hardcoremp/handlers/CommandHandler.java
+++ src/main/java/com/tylermackj/hardcoremp/handlers/CommandHandler.java
@@ -4,6 +4,7 @@ import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.context.CommandContext;
 import com.tylermackj.hardcoremp.HardcoreMP;
 import com.tylermackj.hardcoremp.utils.TeamSuggestionProvider;
+import com.tylermackj.hardcoremp.utils.WordProvider;
 
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.minecraft.server.command.CommandManager;
@@ -15,13 +16,23 @@ public class CommandHandler {
         CommandRegistrationCallback.EVENT.register((dispatcher, registeryAccess, enviroment) -> {
             dispatcher.register(CommandManager.literal(HardcoreMP.MOD_ID)
                 .then(CommandManager.literal("join")
-                    .then(CommandManager.argument("team", StringArgumentType.string())
+                    .then(CommandManager.argument("team", StringArgumentType.word())
                         .suggests(new TeamSuggestionProvider())
                         .executes(CommandHandler::joinTeam)
                     ) 
                 )
                 .then(CommandManager.literal("leave")
                     .executes(CommandHandler::leaveTeam)
+                )
+                .then(CommandManager.literal("create")
+                    .then(CommandManager.literal("manual")
+                        .then(CommandManager.argument("name", StringArgumentType.string())
+                            .executes(CommandHandler::createTeam) 
+                        )
+                    )
+                    .then(CommandManager.literal("automatic")
+                        .executes(CommandHandler::generateTeam) 
+                    )
                 )
             );
         });
@@ -38,5 +49,19 @@ public class CommandHandler {
         context.getSource().sendFeedback(() -> Text.literal("Leaving team"), false);
         context.getSource().getPlayer().getServer().getCommandManager().executeWithPrefix(context.getSource(), "team leave @s");
         return 1;
+    }
+
+    private static int createTeam(CommandContext<ServerCommandSource> context) {
+        String name = StringArgumentType.getString(context, "name");
+        context.getSource().getPlayer().getServer().getCommandManager().executeWithPrefix(context.getSource(), "team add " + name.replaceAll(" ", "_") + " " + name);
+        context.getSource().getPlayer().getServer().getCommandManager().executeWithPrefix(context.getSource(), "team join " + name.replaceAll(" ", "_"));
+        return 1;
+    }
+
+    private static int generateTeam(CommandContext<ServerCommandSource> context) {
+        WordProvider.NamePair namePair = WordProvider.getInstance().getPair();
+        context.getSource().getPlayer().getServer().getCommandManager().executeWithPrefix(context.getSource(), "team add " + namePair.getCodeName() + " \"" + namePair.getDisplayName() + "\"");
+        context.getSource().getPlayer().getServer().getCommandManager().executeWithPrefix(context.getSource(), "team join " + namePair.getCodeName());
+        return 1; 
     }
 }
