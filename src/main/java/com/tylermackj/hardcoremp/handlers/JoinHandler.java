f34d2effeae684f3dd9d53f4c45c9f3494b6698b
TylerMackJ <tylermackj+github@proton.me>
2025-04-17 18:26:38 -0500

    Updated JoinHandler to include team join and create messages

--- src/main/java/com/tylermackj/hardcoremp/handlers/JoinHandler.java
+++ src/main/java/com/tylermackj/hardcoremp/handlers/JoinHandler.java
@@ -1,22 +1,55 @@
 package com.tylermackj.hardcoremp.handlers;
 
-import org.slf4j.Logger;
-
 import com.tylermackj.hardcoremp.HardcoreMP;
 import com.tylermackj.hardcoremp.utils.Utils;
 
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerEntityEvents;
+import net.minecraft.scoreboard.Team;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.ClickEvent;
+import net.minecraft.text.HoverEvent;
+import net.minecraft.text.MutableText;
+import net.minecraft.text.Style;
+import net.minecraft.text.Text;
 
 public class JoinHandler {
-
-    public static final Logger LOGGER = HardcoreMP.LOGGER;
 
     public static void registerEvents() {
         ServerEntityEvents.ENTITY_LOAD.register((entity, world) -> {
             if (entity.isPlayer()) {
-                LOGGER.info("Player joined " + entity.getName() );
-                Utils.checkAttemptUuid((ServerPlayerEntity) entity);
+                HardcoreMP.LOGGER.info("Player joined " + entity.getName());
+                MutableText teamOptionsText = Text.of("Join team:").copy();
+                for (Team team : world.getScoreboard().getTeams()) {
+                    teamOptionsText.append(
+                        Text.of("\n    ").copy().append(
+                            team.getDisplayName().copy().setStyle(
+                                team.getDisplayName().getStyle().withClickEvent(
+                                    new ClickEvent.RunCommand("/hardcore-mp join " + team.getName())
+                                ).withHoverEvent(
+                                    new HoverEvent.ShowText(Text.of("Join ").copy().append(team.getDisplayName()))
+                                ).withUnderline(true)
+                            )
+                        )
+                    );
+                }
+                MutableText teamCreateText = Text.of("Create new team").copy().append(
+                    Text.of("\n    ").copy().append(
+                        Text.of("Manual").copy().setStyle(
+                            Style.EMPTY.withClickEvent(new ClickEvent.SuggestCommand("/hardcore-mp create ")).withUnderline(true)
+                        )
+                    ).append("    ").append(
+                        Text.of("Automatic").copy().setStyle(
+                            Style.EMPTY.withClickEvent(new ClickEvent.RunCommand("/hardcore-mp create automatic")).withUnderline(true)
+                        )
+
+                    )
+                );
+                if (entity.getScoreboardTeam() == null) {
+                    ((ServerPlayerEntity) entity).sendMessage(Text.empty().append(teamOptionsText).append(Text.of("\n")).append(teamCreateText));
+                }
+                else {
+                    Utils.checkAttemptUuid((ServerPlayerEntity) entity);
+                }
             }
         });
     }
--- src/main/java/com/tylermackj/hardcoremp/mixin/TeamJoinMixin.java
+++ src/main/java/com/tylermackj/hardcoremp/mixin/TeamJoinMixin.java
@@ -8,7 +8,6 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import com.tylermackj.hardcoremp.ComponentRegisterer;
 import com.tylermackj.hardcoremp.HardcoreMP;
 import com.tylermackj.hardcoremp.utils.Utils;
 
