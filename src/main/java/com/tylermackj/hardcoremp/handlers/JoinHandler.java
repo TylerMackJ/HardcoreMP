d2f5d7eb78e74e75638bcdb23d3d85fb2928c6c8
TylerMackJ <tylermackj+github@proton.me>
2025-04-17 19:29:03 -0500

    Added some better formatting to team join and create messages

--- src/main/java/com/tylermackj/hardcoremp/handlers/JoinHandler.java
+++ src/main/java/com/tylermackj/hardcoremp/handlers/JoinHandler.java
@@ -1,11 +1,9 @@
 package com.tylermackj.hardcoremp.handlers;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 
 import com.tylermackj.hardcoremp.HardcoreMP;
 import com.tylermackj.hardcoremp.utils.Utils;
-import com.tylermackj.hardcoremp.utils.WordProvider.NamePair;
 
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerEntityEvents;
 import net.minecraft.scoreboard.Team;
@@ -41,7 +39,7 @@ public class JoinHandler {
                     teamNames.add(new NamePair(team.getDisplayName(), team.getName()));
                 }
                 teamNames.sort(null);
-                MutableText teamOptionsText = Text.of("Join team:").copy();
+                MutableText teamOptionsText = Text.of("Join team:").copy().setStyle(Style.EMPTY.withBold(true));
                 for (NamePair name : teamNames) {
                     teamOptionsText.append(
                         Text.of("\n    ").copy().append(
@@ -51,18 +49,31 @@ public class JoinHandler {
                                 ).withHoverEvent(
                                     new HoverEvent.ShowText(Text.of("Join ").copy().append(name.name))
                                 ).withUnderline(true)
+                                .withColor(name.name.hashCode() | 0x404040)
+                                .withBold(false)
                             )
                         )
                     );
                 }
-                MutableText teamCreateText = Text.of("Create team:").copy().append(
+                MutableText teamCreateText = Text.of("Create team:").copy()
+                .setStyle(
+                    Style.EMPTY.withBold(true)
+                ).append(
                     Text.of("\n    ").copy().append(
                         Text.of("Manual").copy().setStyle(
-                            Style.EMPTY.withClickEvent(new ClickEvent.SuggestCommand("/" + HardcoreMP.MOD_ID + " create ")).withUnderline(true)
+                            Style.EMPTY.withClickEvent(new ClickEvent.SuggestCommand("/" + HardcoreMP.MOD_ID + " create manual "))
+                            .withUnderline(true)
+                            .withColor(0xFFFF00)
+                            .withHoverEvent(new HoverEvent.ShowText(Text.of("Create team with custom name")))
+                            .withBold(false)
                         )
                     ).append("    ").append(
                         Text.of("Automatic").copy().setStyle(
-                            Style.EMPTY.withClickEvent(new ClickEvent.RunCommand("/" + HardcoreMP.MOD_ID + " create automatic")).withUnderline(true)
+                            Style.EMPTY.withClickEvent(new ClickEvent.RunCommand("/" + HardcoreMP.MOD_ID + " create automatic"))
+                            .withUnderline(true)
+                            .withColor(0xFFFF00)
+                            .withHoverEvent(new HoverEvent.ShowText(Text.of("Create team with generated name")))
+                            .withBold(false)
                         )
 
                     )
